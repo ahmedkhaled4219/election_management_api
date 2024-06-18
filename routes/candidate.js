8630a945ed2a0f9ac96028171fb34d25268273dd
NooranTarek <noort.mohamed@gmail.com>
2024-06-18 14:08:02 +0300

    adding authentication

--- routes/candidate.js
+++ routes/candidate.js
@@ -1,11 +1,12 @@
 import express from "express";
 import * as candidateController from "../controllers/candidate.js";
+import { isAuthenticated } from "../middlewares/authentication.js";
 
 
 
 const candidateRouter=express.Router();
 
-candidateRouter.post('/createCandidate',candidateController.createCandidate);
+candidateRouter.post('/createCandidate',isAuthenticated,candidateController.createCandidate);
 
 
 export default candidateRouter;
