25586cf29d470f865de2c502719791203d470204
NooranTarek <noort.mohamed@gmail.com>
2024-06-18 13:04:23 +0300

    adding citizen signin

--- controllers/citizen.js
+++ controllers/citizen.js
@@ -1,6 +1,7 @@
 import { Citizen } from "../models/citizen.js";
 import { catchAsyncErr } from "../utilities/catchError.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 
 
@@ -20,5 +21,16 @@ const signUp = catchAsyncErr(async (req, res) => {
 
     res.status(201).json({ message: "Inserted successfully", citizen: newCitizen });
 });
-
-export {signUp};
+const signin = catchAsyncErr(async (req, res) => {
+    const { ssn, password } = req.body;
+    let citizen = await Citizen.findOne({ ssn });
+  
+    if (!citizen || !(await bcrypt.compare(password, citizen.password))) {
+      return res.json({ message: "incorrect ssn or password" });
+    }
+    citizen["password"] = undefined;
+    var token = jwt.sign({ citizen }, process.env.JWT_KEY);
+    var role=citizen.role;
+    res.json({ message: "login successfully", token,role });
+  });
+export {signUp,signin};
--- routes/citizen.js
+++ routes/citizen.js
@@ -6,6 +6,7 @@ import * as citizenController from "../controllers/citizen.js";
 const citizenRouter=express.Router();
 
 citizenRouter.post('/signup',citizenController.signUp);
+citizenRouter.post('/signin',citizenController.signin);
 
 
 export default citizenRouter;
