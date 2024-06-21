68ea4070fae85c2eacad534e618cd197556825ef
NooranTarek <noort.mohamed@gmail.com>
2024-06-21 13:33:57 +0300

    adding pagination function to be used anywhere

---
+++ utilities/pagination.js
@@ -0,0 +1,18 @@
+export const paginate = async (model, page, limit) => {
+    const pageNumber = parseInt(page, 10) || 1;
+    const pageSize = parseInt(limit, 10) || 10;
+
+    const skip = (pageNumber - 1) * pageSize;
+    const total = await model.countDocuments({});
+
+    const pages = Math.ceil(total / pageSize);
+    const results = await model.find({}).skip(skip).limit(pageSize);
+
+    return {
+        page: pageNumber,
+        limit: pageSize,
+        total,
+        pages,
+        results,
+    };
+};
