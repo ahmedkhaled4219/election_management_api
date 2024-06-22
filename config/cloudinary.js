f4bebb0dd5106195d45467df517e70162f0a923f
Ahmed Khaled <ahmedkhaled4219@gmail.com>
2024-06-22 20:34:06 +0300

    Uploading CriminalRecord and logoImage on cloudinary

---
+++ config/cloudinary.js
@@ -0,0 +1,27 @@
+import cloudinary from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
+
+cloudinary.v2.config({
+  cloud_name: 'dimdv1umm',
+  api_key: '298367857295941',
+  api_secret: 'C1fjaW3Rwrc11sT5mHupmKUtSac',
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary.v2,
+  params: async (req, file) => {
+    let folder = 'default';
+    if (file.fieldname === 'criminalRecord') folder = 'criminal_records';
+    if (file.fieldname === 'logoImage') folder = 'logo_images';
+
+    return {
+      folder: folder,
+      resource_type: 'auto', 
+    };
+  },
+});
+
+const upload = multer({ storage: storage });
+
+export { cloudinary, upload };
