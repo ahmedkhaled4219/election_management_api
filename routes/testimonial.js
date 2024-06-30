b4bddc0ae66c51d38cd3fc0826e1d05d0aff0cfc
Nooran Tarek <102181986+NooranTarek@users.noreply.github.com>
2024-06-30 21:26:47 +0300

    Update testimonial.js

--- routes/testimonial.js
+++ routes/testimonial.js
@@ -5,7 +5,7 @@ import { allowedTo } from '../middlewares/authorization.js';
 
 const testimonialRouter = express.Router();
 
-testimonialRouter.post('/',isAuthenticated,allowedTo("citizen"), testimonialController.createTestimonial);
+testimonialRouter.post('/',isAuthenticated,allowedTo("citizen","candidate"), testimonialController.createTestimonial);
 testimonialRouter.get('/', testimonialController.getAllTestimonials);
 testimonialRouter.get('/election/:electionId', testimonialController.getTestimonialsByElection);
 testimonialRouter.get('/citizen/:citizenId',isAuthenticated, testimonialController.getTestimonialsByCitizen);
